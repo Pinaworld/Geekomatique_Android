f766a5edd00e06b5be3c6444a62b2edea5feee2e
Pinaworld <pinben02@gmail.com>
2020-06-02 12:26:15 +0200

    + 2 Activities again

---
+++ app/src/main/java/com/example/geekomatique/ModifyAppointment.java
@@ -0,0 +1,13 @@
+package com.example.geekomatique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_appointment);
+    }
+}
