a0b78c96e415c49751f3c1319d573baa9d2d5814
Pinaworld <pinben02@gmail.com>
2020-06-24 11:21:01 +0200

    @string added + fixed activities

--- app/src/main/java/com/example/geekomatique/Disponibilities.java
+++ app/src/main/java/com/example/geekomatique/Disponibilities.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Disponibilities extends AppCompatActivity {
 
@@ -17,4 +19,20 @@ public class Disponibilities extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
+
+
+    Spinner dropdown = findViewById(R.id.FirstDay);
+
+    String[] itemsFirstDay = new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+    String[] itemsLastDay = new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+    String[] itemsFirstHour = new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+    String[] itemsLastHour = new String[]{"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"};
+    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+    ArrayAdapter<String> adapterFirstDay = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsFirstDay);
+    ArrayAdapter<String> adapterLastDay = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsLastDay);
+    ArrayAdapter<String> adapterFirstHour = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsFirstHour);
+    ArrayAdapter<String> adapterLastHour = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, itemsLastHour);
+//set the spinners adapter to the previously created one.
+//dropdown.setAdapter(adapter);
 }
--- app/src/main/java/com/example/geekomatique/ListAppointments.java
+++ app/src/main/java/com/example/geekomatique/ListAppointments.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ListAppointments extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listAppointments);
+        setContentView(R.layout.activity_list_appointments);
+
+        String date_n = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+        TextView date  = (TextView) findViewById(R.id.actualDate);
+        date.setText(date_n);
     }
 
     public void ReturnAppointmentActivity(View view) {
