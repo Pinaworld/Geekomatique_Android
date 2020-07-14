0f94e021d873ed24cf1e94ffb22c2b578d6019fd
Pinaworld <pinben02@gmail.com>
2020-07-14 10:45:18 +0200

    modelRole

---
+++ app/src/main/java/com/example/geekomatique/Models/RoleModel.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020
+ * Project: Geekomatique
+ * File : RoleModel.java
+ * Edited by pinbe
+ */
+
+package com.example.geekomatique.Models;
+
+public class RoleModel {
+
+
+}
